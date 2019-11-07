245a9cce5c4ae26fe16af0aeaf5d67c259ced528
arisulistio37 <arisulistio37@gmail.com>
2019-11-07 19:52:28 +0700

    Initial commit

---
+++ src/if1/pkg10118030/latihan57/vehicle/Skateboard.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan57.vehicle;
+
+/**
+ *
+ * @author Ari Sulistio
+ */
+public class Skateboard extends Vehicle{
+    private double myBoardLength;
+    
+    public Skateboard (){
+        System.out.println("Skateboard");
+    }
+
+    public double getMyBoardLength() {
+        return myBoardLength;
+    }
+
+    public void setMyBoardLength(double myBoardLength) {
+        this.myBoardLength = myBoardLength;
+    }
+}
